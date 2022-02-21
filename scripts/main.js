0caab8b27285661373fa356e0db0fcfa8e0e4f10
Juan-Casanova <casanovajm199621@gmail.com>
2022-02-20 22:31:54 -0600

    es funcional el skill, y about me

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,37 @@ const typed = new Typed('.typed', {
     contentType: 'html'
 
 });
+
+/*************EVENTO DE CAMBIO DE IMAGEN DE FONDO**************/
+
+/**************Se establecen variables **************/
+const containerSkills = document.getElementById("container-skills");
+const col1 = document.getElementById("col-1");
+const col2 = document.getElementById("col-2");
+const col3 = document.getElementById("col-3");
+const col4 = document.getElementById("col-4");
+
+/*******Eventos del div 1*************/
+col1.addEventListener("mouseover", function(e){
+    containerSkills.style.backgroundImage = "url('https://res.cloudinary.com/pelos-locos/image/upload/v1645242335/Portafolio/Back.jpg')";
+    containerSkills.style.transitionDuration= "1s";
+})
+col1.addEventListener("mouseout", function(e){
+    containerSkills.style.backgroundImage = "";
+})
+
+/*******Eventos del div 2*************/
+col2.addEventListener("mouseover", function(e){
+    containerSkills.style.backgroundImage = "url('https://res.cloudinary.com/pelos-locos/image/upload/v1645242335/Portafolio/Front.jpg')";
+})
+col2.addEventListener("mouseout", function(e){
+    containerSkills.style.backgroundImage = "";
+})
+
+/*******Eventos del div 3*************/
+col3.addEventListener("mouseover", function(e){
+    containerSkills.style.backgroundImage = "url('https://res.cloudinary.com/pelos-locos/image/upload/v1645242335/Portafolio/Extras.jpg')";
+})
+col3.addEventListener("mouseout", function(e){
+    containerSkills.style.backgroundImage = "";
+})
