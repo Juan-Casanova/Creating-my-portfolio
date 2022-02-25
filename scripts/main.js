892f4f0f87ba35854843702e57655270dfbcb0b8
Juan-Casanova <casanovajm199621@gmail.com>
2022-02-25 13:48:25 -0600

    se termina pagina

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+
+const containerSkills = document.getElementById("container-skills");
+const col1 = document.getElementById("col-11");
+const col2 = document.getElementById("col-22");
+const col3 = document.getElementById("col-33");
+
 const typed = new Typed('.typed', {
 
     strings: [
@@ -19,17 +25,35 @@ const typed = new Typed('.typed', {
 
 });
 
+const typed2 = new Typed('.typed-movil', {
+
+    strings: [
+        '<i class="names2"> FRONT DEVELOPER</i>', 
+        '<i class="names2"> WEB DEVELOPER</i>', 
+        '<i class="names2"> GAME DEVELOPER</i>'
+    ],
+
+    typeSpeed: 75,//tiempop en poner una letra
+    startDelay: 300,//tempo de retarso para iniciar animacion
+    backSpeed: 75, //tiempo en borar la palabra
+    shuffle: true,
+    backDelay: 3000, //tiempo para esperar borarr la palabra
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|', 
+    contentType: 'html'
+
+});
+
+
 /*************EVENTO DE CAMBIO DE IMAGEN DE FONDO**************/
 
 /**************Se establecen variables **************/
-const containerSkills = document.getElementById("container-skills");
-const col1 = document.getElementById("col-1");
-const col2 = document.getElementById("col-2");
-const col3 = document.getElementById("col-3");
-const col4 = document.getElementById("col-4");
+
 
 /*******Eventos del div 1*************/
-col1.addEventListener("mouseover", function(e){
+/*col1.addEventListener("mouseover", function(e){
     containerSkills.style.backgroundImage = "url('https://res.cloudinary.com/pelos-locos/image/upload/v1645242335/Portafolio/Back.jpg')";
     containerSkills.style.transitionDuration= "1s";
 })
@@ -38,7 +62,7 @@ col1.addEventListener("mouseout", function(e){
 })
 
 /*******Eventos del div 2*************/
-col2.addEventListener("mouseover", function(e){
+/*col2.addEventListener("mouseover", function(e){
     containerSkills.style.backgroundImage = "url('https://res.cloudinary.com/pelos-locos/image/upload/v1645242335/Portafolio/Front.jpg')";
 })
 col2.addEventListener("mouseout", function(e){
@@ -46,9 +70,29 @@ col2.addEventListener("mouseout", function(e){
 })
 
 /*******Eventos del div 3*************/
-col3.addEventListener("mouseover", function(e){
+/*col3.addEventListener("mouseover", function(e){
     containerSkills.style.backgroundImage = "url('https://res.cloudinary.com/pelos-locos/image/upload/v1645242335/Portafolio/Extras.jpg')";
 })
 col3.addEventListener("mouseout", function(e){
     containerSkills.style.backgroundImage = "";
-})
+})*/
+
+function changeImg1() {
+    containerSkills.style.backgroundImage = "url('https://res.cloudinary.com/pelos-locos/image/upload/v1645242335/Portafolio/Back.jpg')";
+    containerSkills.style.transitionDuration= "1s";
+}
+
+function quitImg() {
+    containerSkills.style.backgroundImage = "";
+    containerSkills.style.transitionDuration= "1s";
+}
+
+function changeImg2 () {
+    containerSkills.style.backgroundImage = "url('https://res.cloudinary.com/pelos-locos/image/upload/v1645242335/Portafolio/Front.jpg')";
+    containerSkills.style.transitionDuration= "1s";
+}
+
+function changeImg3 () {
+    containerSkills.style.backgroundImage = "url('https://res.cloudinary.com/pelos-locos/image/upload/v1645242335/Portafolio/Extras.jpg')";
+    containerSkills.style.transitionDuration= "1s";
+}
